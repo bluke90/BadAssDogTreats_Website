0eb80ced1ae8b034511298e6f12e2b02adeff807
blake <blake@BLAKESPC>
2022-08-09 20:12:37 -0500

    Add project files.

---
+++ BadAssDogTreats_Website/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BadAssDogTreats_Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
