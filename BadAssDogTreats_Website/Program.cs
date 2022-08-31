16ea40f5d09e946b02692396129dc975dd560816
Blake Ganzerla <blake.ganzerla@outlook.com>
2022-08-31 12:17:46 -0500

    Push Progress

--- BadAssDogTreats_Website/Program.cs
+++ BadAssDogTreats_Website/Program.cs
@@ -1,6 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Http;
+
+
 using BadAssDogTreats_Website.Data;
+using Microsoft.Identity.Web.UI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +19,22 @@ builder.Services.AddDbContext<WebContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("productionDb") ?? throw new InvalidOperationException("Connection string 'WebContext' not found.")));
 
 
+//Azure AD authentication
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
+builder.Services.AddControllersWithViews(options => {
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+builder.Services.AddRazorPages().AddMicrosoftIdentityUI().AddRazorPagesOptions(options => {
+    options.Conventions.AllowAnonymousToPage("/index");
+    options.Conventions.AuthorizeFolder("/backend/Store");
+    options.Conventions.AuthorizeFolder("/backend/ToDos");
+});
+
+
+// Build
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +49,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
